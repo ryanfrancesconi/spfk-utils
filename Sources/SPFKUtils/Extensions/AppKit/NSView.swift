1f0bc9c545a2a67f8f9d23fc178041612a45b09f
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-03-08 18:29:14 -0700

    refactoring and cleanup after components were made

--- Sources/SPFKUtils/Extensions/AppKit/NSView.swift
+++ Sources/SPFKUtils/Extensions/AppKit/NSView.swift
@@ -48,7 +48,7 @@
             let value = (otherFrame.width / 2) - (frame.width / 2)
             frame.origin.x = otherFrame.origin.x + value
         }
-        
+
         public func alignVertical(with otherFrame: NSRect) {
             let value = (otherFrame.height / 2) - (frame.height / 2)
             frame.origin.y = otherFrame.origin.y + value
