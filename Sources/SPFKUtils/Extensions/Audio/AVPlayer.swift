c370cf7be87f77259f3d5f66f592c298f8744146
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-03-12 12:26:53 -0700

    moved to spfk-audio-base

--- Sources/SPFKUtils/Extensions/Audio/AVPlayer.swift
+++
@@ -1,9 +0,0 @@
-// Copyright Ryan Francesconi. All Rights Reserved. Revision History at https://github.com/ryanfrancesconi/spfk-utils
-
-import AVFoundation
-
-extension AVPlayer {
-    public var isPlaying: Bool {
-        (rate != 0) && (error == nil)
-    }
-}
