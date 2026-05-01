e00ef792803f1fd7b05c285344360520133fa8b9
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-05-01 07:54:21 -0700

    Add NSTextField.textSize extension; rename file to NSText

--- Sources/SPFKUtils/Extensions/AppKit/NSText.swift
+++ Sources/SPFKUtils/Extensions/AppKit/NSText.swift
@@ -22,4 +22,17 @@
         }
     }
 
+    extension NSTextField {
+        public var textSize: NSSize {
+            guard let font else {
+                return frame.size
+            }
+
+            let fontAttributes = [NSAttributedString.Key.font: font]
+            let size = stringValue.size(withAttributes: fontAttributes)
+
+            return size
+        }
+    }
+
 #endif
