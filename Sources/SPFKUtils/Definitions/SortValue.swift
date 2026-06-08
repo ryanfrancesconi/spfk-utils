23658164fa98393e85843bef3e36c75af0be379d
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-06-08 14:21:14 -0700

    Add SortValue typed sort enum

---
+++ Sources/SPFKUtils/Definitions/SortValue.swift
@@ -0,0 +1,32 @@
+// Copyright Ryan Francesconi. All Rights Reserved.
+
+import Foundation
+
+/// A typed sort value used to compare table rows across columns of different data types.
+public enum SortValue: Hashable, Sendable {
+    case string(String)
+    case double(Double)
+    case integer(Int)
+}
+
+extension SortValue: Comparable {
+    public static func < (lhs: SortValue, rhs: SortValue) -> Bool {
+        switch (lhs, rhs) {
+        case (.string(let a), .string(let b)):
+            return a.localizedStandardCompare(b) == .orderedAscending
+        case (.double(let a), .double(let b)):
+            return a < b
+        case (.integer(let a), .integer(let b)):
+            return a < b
+        case (.integer(let a), .double(let b)):
+            return Double(a) < b
+        case (.double(let a), .integer(let b)):
+            return a < Double(b)
+        case (.string, .double), (.string, .integer):
+            // Numerics sort before strings in cross-type comparisons
+            return false
+        case (.double, .string), (.integer, .string):
+            return true
+        }
+    }
+}
