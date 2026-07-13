b7609730714a990a50c9e36a6088eaff59115d10
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-07-12 18:14:01 -0700

    Update dependency version minimums to current tags

--- Package.swift
+++ Package.swift
@@ -14,8 +14,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/ryanfrancesconi/spfk-audio-base", from: "1.6.0"),
-        .package(url: "https://github.com/ryanfrancesconi/spfk-filesystem", from: "1.2.1"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-audio-base", from: "1.6.1"),
+        .package(url: "https://github.com/ryanfrancesconi/spfk-filesystem", from: "1.2.2"),
         .package(url: "https://github.com/ryanfrancesconi/spfk-testing", from: "1.1.0"),
         .package(url: "https://github.com/tadija/AEXML", from: "4.6.0"),
         .package(url: "https://github.com/rnine/Checksum", from: "1.0.2"),
