377cb92e0c2c281891d7be3ca7ffe7104aa11707
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-07-19 17:11:37 -0700

    Fix source file copyright headers to reference this package
    Headers were pointing at the wrong repo (often the package this one
    was split out of), missing the /ryanfrancesconi/<repo> revision-history
    URL entirely, or missing the header line altogether.

--- Sources/SPFKUtils/Definitions/SortValue.swift
+++ Sources/SPFKUtils/Definitions/SortValue.swift
@@ -1,4 +1,4 @@
-// Copyright Ryan Francesconi. All Rights Reserved.
+// Copyright Ryan Francesconi. All Rights Reserved. Revision History at https://github.com/ryanfrancesconi/spfk-utils
 
 import Foundation
 
